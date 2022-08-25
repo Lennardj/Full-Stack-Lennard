93579bf72abdd609e01befd44f3e2ca974c73e30
Lennardj <lennardvincentjohn@gmail.com>
2022-08-26 04:28:44 +1200

    First commit

---
+++ Week 2/Mission_1/javaScript/app.js
@@ -0,0 +1,15 @@
+console.log("Hello World from an external Javascript file");
+// const firstName = "Lennard";
+// const lastName = "John"
+// let year = 2020;
+// console.log(firstName + " " + lastName + "This is the year" + " " + year);
+// year = 2025;
+// console.log(firstName + " " + lastName + "This is the year" + " " + year);
+// let firstName = "Lennard", lastName = "John"
+// console.log(firstName, lastName);
+
+const firstAndLastName = "Lennard John";
+let age = "32";
+const x = 5; /* this is a comment */
+const ageCalc = 1989-2022;
+console.log( age, ageCalc)
